f88a88dd3d56d921762842ce99758e2c105bf93d
guff192 <kochkons1@ya.ru>
2021-09-15 16:18:06 +0300

    Created file model

--- cloud/models.py
+++ cloud/models.py
@@ -1,5 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class File(models.Model):
-    pass
+    file = models.FileField(upload_to='files', help_text='Файл')
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=255, help_text='Имя файла')
+    FILE_TYPES = (
+        ('i', 'image'),
+        ('m', 'music'),
+        ('v', 'video'),
+        ('d', 'document'),
+        ('p', 'presentation'),
+        ('t', 'table'),
+    )
+    file_type = models.CharField(max_length=1, choices=FILE_TYPES, help_text='Тип файла')
+    slug = models.SlugField(unique=True, null=True)
+    public = models.BooleanField(help_text='Доступен для неавторизованных пользователей', default=False)
